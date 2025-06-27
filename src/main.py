4e16e0af2446d471c1a722507746f6a05e4038e1
Loris Sogliuzzo <sogliuzzo.loris@gmail.com>
2025-06-27 16:36:44 +0200

    Creation de l'evaluator et du generator

--- src/main.py
+++ src/main.py
@@ -1,2 +1,22 @@
-from src.individu import Lampe
+from individu import Lampe
+from model import random_generator, generator, evaluator
 
+import inspyred
+import numpy as np
+import matplotlib.pyplot as plt
+
+nsga2 = inspyred.ec.emo.NSGA2(random=random_generator())
+nsga2.terminator = inspyred.ec.terminators.evaluation_termination
+nsga2.variator = []
+
+pareto_front = nsga2.evolve(
+    generator = generator,
+    evaluator=evaluator,
+
+    # args
+    population_size = 100,
+    group_min = 1,
+    group_max = 10,
+    radius = 0.2,
+    fitness_lamp = {}
+)
---
+++ src/model.py
@@ -0,0 +1,60 @@
+from individu import Lampe
+
+import inspyred
+import numpy as np
+
+import random
+
+def random_generator():
+    return random.Random()
+
+def generator(random: random, args: dict):
+    population = []
+
+    population_size = args["population_size"]
+    r = args["radius"]
+
+    for _ in range(population_size):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        r = r
+        lampe = Lampe(x, y, r)
+        population.append(lampe)
+    return population
+
+def evaluator(candidates, args):
+    group_min = args["group_min"]
+    group_max = args["group_max"]
+    fitness_lamp = {id(candidate) : -np.inf for candidate in candidates}
+
+    # Génération de la taille des groupes
+    groups_size = []
+    candidates_size = len(candidates)
+    while candidates_size > group_min:
+        size = random.randint(group_min, min(group_max, candidates_size))
+        groups_size.append(size)
+        candidates_size -= size
+
+    if candidates_size != 0:
+        groups_size.append(candidates_size)
+
+    # Génération des groupes
+    groups = []
+    candidates_to_visite = candidates.copy()
+    for group_size in groups_size:
+        group = []
+        for _ in range(group_size):
+            group.append(candidates_to_visite.pop())
+        groups.append(group)
+
+    return groups
+
+pop = generator(random.Random(), {"population_size": 100, "radius" : 0.2})
+groups = evaluator(pop, {"group_min" : 1, "group_max" : 10})
+total_size = 0
+for group in groups:
+    print(f"Nouveau group, taille {len(group)}")
+    for lamp in group:
+        print(lamp.x, lamp.y, lamp.r)
+        total_size += 1
+print(total_size)
